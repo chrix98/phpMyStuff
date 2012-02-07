8b03a1ee825955514d5615bdff9505908470e005
Chrix98 <chrix98@gmail.com>
2012-02-07 22:54:11 +0000

    adding language files (though not having translations and not being complete just yet)

---
+++ application/i18n/de.php
@@ -0,0 +1,20 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Translation file in language: de
+ * Automatically generated from previous translation file.
+ */
+return array (
+  'You\'re not logged in' => 'You\'re not logged in',
+  'Home' => 'Home',
+  'Register' => 'Register',
+  'Log in' => 'Log in',
+  'About' => 'About',
+  'Contact' => 'Contact',
+  'Dutch' => 'Dutch',
+  'English' => 'English',
+  'French' => 'French',
+  'German' => 'German',
+  'Polish' => 'Polish',
+  'Language' => 'Language',
+  'User Guide' => 'Handbuch',
+);
